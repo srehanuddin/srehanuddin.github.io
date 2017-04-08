f71db104e6971fa17201c82afe567831d10aff2f
srehanuddin <s.rehanuddin@gmail.com>
2017-04-09 01:53:48 +0500

    Fix

--- service-worker.js
+++ service-worker.js
@@ -138,7 +138,7 @@ self.addEventListener('activate', function(e) {
 self.addEventListener('fetch', function(e) {
   console.log('[Service Worker] Fetch', e.request.url);
   //var dataUrl = 'https://query.yahooapis.com/v1/public/yql';
-  var dataUrl = 'http://rehanuddin.me';
+  var dataUrl = 'https://rehanuddin.me';
   if (e.request.url.indexOf(dataUrl) > -1) {
     /*
      * When the request URL contains dataUrl, the app is asking for fresh
